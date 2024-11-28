d80779b01a189c6f3ef86124b62ae46795d865c3
Mark-Mikaeel <mikaeelmark@gmail.com>
2024-11-27 19:49:38 -0500

    fix windows linux env setup

--- linux_env_setup/windows_set_linux_env.ps1
+++ linux_env_setup/windows_set_linux_env.ps1
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # To set up linux, run this command in your vs powershell:
-  # .\run_docker.ps1
+  # cd linux_env_setup
+  # .\windows_set_linux_env.ps1
 
 # Run an Ubuntu Docker container interactively, mount the current directory as /workspace
 docker run -it -v ${PWD}:/workspace ubuntu bash -c "
