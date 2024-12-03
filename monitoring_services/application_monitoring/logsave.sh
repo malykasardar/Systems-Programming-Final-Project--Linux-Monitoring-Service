407691b8192259567ffc2b367c3e7ae7a961ce05
Mark-Mikaeel <mikaeelmark@gmail.com>
2024-12-02 19:55:06 -0500

    add network and security service tracking

---
+++ monitoring_services/application_monitoring/logsave.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Output file to save logs
+OUTPUT_FILE="$HOME/Software-Programming-final-project/monitor_logs/application_logs/sys_utils"
+
+# Notification of the monitoring process
+echo "Saving command outputs to: $OUTPUT_FILE"
+
+# Save the output in a file (eg: `df -h` for disk usage)
+while true; do
+  echo "Running command 'df -h' to display disk usage: " | tee -a "$OUTPUT_FILE"
+  df -h | tee -a "$OUTPUT_FILE"
+  echo "-------------------------------" | tee -a "$OUTPUT_FILE"
+  sleep 60 
+done
